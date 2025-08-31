0c0df1d35df73c9cde8e7cb8498b42fd3a478967
CUI Hao <cuihao.leo@gmail.com>
2025-08-31 10:01:23 -0700

    Handler for HackerNews

--- src/index.ts
+++ src/index.ts
@@ -23,10 +23,61 @@ async function handleWeixin(url: URL) {
   });
 }
 
+function escapeHtml(unsafe: string) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+async function handleHackerNews(url: URL) {
+  const res = await fetch(url);
+
+  const hackerNewsHandler = {
+    title: '',
+    url: '',
+    element(element: Element) {
+      const href = element.getAttribute('href') || '';
+      this.url = new URL(href, url).href;
+    },
+    text(text: Text) {
+      this.title += text.text;
+    },
+  };
+  await new HTMLRewriter()
+    .on('span.titleline>a', hackerNewsHandler)
+    .transform(res)
+    .text();
+
+  const title = escapeHtml(`${hackerNewsHandler.title}`);
+  const newHtml = `
+    <html>
+      <head>
+        <title>${title}</title>
+        <meta property="og:title" content="${title}"/>
+        <meta property="og:description" content=""/>
+        <meta property="og:site_name" content="Hacker News"/>
+        <meta property="og:image" content="https://news.ycombinator.com/y18.svg"/>
+      </head>
+      <body>
+      </body>
+    </html>
+  `;
+
+  return new Response(newHtml, {
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8',
+    },
+  });
+}
+
 const HANDLER_MAP: { [key: string]: (url: URL) => Promise<Response> } = {
   'mp.weixin.qq.com': handleWeixin,
   'twitter.com': handleTwitter,
   'x.com': handleTwitter,
+  'news.ycombinator.com': handleHackerNews,
 };
 
 export default {
